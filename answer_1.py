33729b6a0d96830ed974f449943429f41725a3f2
Arnold Samuel Chan <arnold.chan@lndata.com>
2022-03-04 16:44:51 +0700

    test(no.1): :white_check_mark: update more strict test

--- answer_1.py
+++ answer_1.py
@@ -8,15 +8,21 @@ def answer(string):
 if __name__ == "__main__":
     result = answer("abc")
     correct_answer = ["ab", "ac", "ba", "bc", "ca", "cb"]
+    assert len(result) == len(correct_answer)
     assert all([each in correct_answer for each in result])
+    assert all([each in result for each in correct_answer])
     print("PASSED 1")
 
     result = answer("bba")
     correct_answer = ["bb", "ba", "ab"]
+    assert len(result) == len(correct_answer)
     assert all([each in correct_answer for each in result])
+    assert all([each in result for each in correct_answer])
     print("PASSED 2")
 
     result = answer("bbbcd")
     correct_answer = ["bb", "bc", "bd", "cb", "cd", "db", "dc"]
+    assert len(result) == len(correct_answer)
     assert all([each in correct_answer for each in result])
+    assert all([each in result for each in correct_answer])
     print("PASSED 3")
